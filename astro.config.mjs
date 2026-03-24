dbbd26f7e94dbad05664fcbdedc4605049d1c473
chrischriscris <18-10892@usb.ve>
2026-03-24 11:50:22 -0400

    moved from vercel to cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
